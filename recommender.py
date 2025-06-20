81e3e380ea9dab146517ce1c69b5f797685589b3
hui <103429373+oneJue@users.noreply.github.com>
2025-06-20 17:13:32 +0800

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def rerank_paper(candidate:list[ArxivPaper],corpus:list[dict],model:str='avsolatorio/GIST-small-Embedding-v0') -> list[ArxivPaper]:
     encoder = SentenceTransformer(model)
     #sort corpus by date, from newest to oldest
-    corpus = sorted(corpus,key=lambda x: datetime.strptime(x['dateAdded'], '%Y-%m-%dT%H:%M:%SZ'),reverse=True)
+    corpus = sorted(corpus,key=lambda x: datetime.strptime(x[data]['dateAdded'], '%Y-%m-%dT%H:%M:%SZ'),reverse=True)
     time_decay_weight = 1 / (1 + np.log10(np.arange(len(corpus)) + 1))
     time_decay_weight = time_decay_weight / time_decay_weight.sum()
     corpus_feature = encoder.encode([paper['abstractNote'] for paper in corpus])
